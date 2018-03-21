f50f300e159e18aba40333de6d31fa25e890c9a8
crazy rabbidz <hugsy@users.noreply.github.com>
2018-03-20 22:50:20 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 source $(dirname $0)/vars.sh
 
-TAC_FILE="$(dirname $0)/python/start-all-pot.tac"
+TAC_FILE="`realpath $(dirname $0)`/python/start-all-pot.tac"
 touch /var/log/{ssh,ftp,telnet}-pot.log
 chown 1:1 /var/log/{ssh,ftp,telnet}-pot.log
 twistd --uid=1 --gid=1 --logfile=${TWISTED_LOG} --pidfile=${TWISTED_PID} --python=${TAC_FILE}
