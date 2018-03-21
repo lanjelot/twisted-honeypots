feaedbf4b3028e28b8f91049d8a94a553fad4dda
crazy rabbidz <hugsy@users.noreply.github.com>
2018-03-20 22:52:08 -0700

    Update vars.sh

--- vars.sh
+++ vars.sh
@@ -1,7 +1,6 @@
-MYSQL_USER=pot_user
-MYSQL_DB=pot_db
-MYSQL_PWD=password
-TWISTED_PID=/var/run/twistd-pot.pid
-TWISTED_LOG=/var/log/twistd-pot.log
-
+export MYSQL_USER=pot_user
+export MYSQL_DB=pot_db
+export MYSQL_PWD=password
+export TWISTED_PID=/var/run/twistd-pot.pid
+export TWISTED_LOG=/var/log/twistd-pot.log
 export PYTHONPATH=${PYTHONPATH}:/opt/twisted-honeypots/python
